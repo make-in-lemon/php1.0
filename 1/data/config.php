f08267256084a39a4df4f0d7595ad4d0df0e1e1a
505293192 <505293192@qq.com>
2016-01-29 02:31:42 +0000

    29-Jan-16 调整数据库连接 2

--- 1/data/config.php
+++ 1/data/config.php
@@ -37,14 +37,15 @@ define('ADMIN_PATH','admin');
 define('M_PATH','m');*/
 
 	header("Content-type:text/html;charset=utf-8");
-	$con=mysql_connect(SAE_MYSQL_HOST_M.":".SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
-    mysql_select_db(SAE_MYSQL_DB,$con);
-	if(!$con){echo "错误!" .mysql_error();}
+	$dou = new Action(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS, SAE_MYSQL_DB, $prefix, DOU_CHARSET);
+	//$con=mysql_connect(SAE_MYSQL_HOST_M.":".SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+    mysql_select_db(SAE_MYSQL_DB,$dou);
+	if(!$dou){echo "错误!" .mysql_error();}
 	
-	//mysql_select_db("users",$con);  //数据库名字
+	//mysql_select_db("users",$dou);  //数据库名字
 
-	mysql_query("SET character_set_connection=utf8, character_set_results=utf8, character_set_client=utf8", $con);
-	mysql_query("SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO'", $con);
+	mysql_query("SET character_set_connection=utf8, character_set_results=utf8, character_set_client=utf8", $dou);
+	mysql_query("SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO'", $dou);
 
 
 
